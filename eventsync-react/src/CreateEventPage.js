4a6a78f56fa3992be30b227a60239c055e307a6b
byardHK <ByardHK21@GCC.edu>
2024-12-02 20:18:32 -0500

    Added basic ability to show events and some visuals

--- eventsync-react/src/CreateEventPage.js
+++ eventsync-react/src/CreateEventPage.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 const CreateEventPage = () => {
     return <>
-        <p>Create Event Page</p>
+        <h1>Create Event Page</h1>
+        <Box
+            component="form"
+            sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
+            noValidate
+            autoComplete="off"
+        >
+        <TextField id="outlined-basic" label="Title" variant="outlined" />
+        <TextField id="outlined-basic" label="Date" variant="outlined" />
+        <TextField id="outlined-basic" label="Location" variant="outlined" />
+        </Box>
+        <Button variant="contained" align="center">Submit</Button>
+        <br></br>
         <Link to="/">Home Page</Link>
     </>;
 };
