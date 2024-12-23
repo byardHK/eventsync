6e98912c51bc62d67a582dc21a4e2c6c1abf28d6
byardHK <ByardHK21@GCC.edu>
2024-12-22 19:58:59 -0500

    Styled basic home page and bottom nav bar

---
+++ eventsync-react/src/BottomNavBar.tsx
@@ -0,0 +1,38 @@
+import { Box, Button } from "@mui/material";
+import { Link } from "react-router-dom";
+import HomeIcon from '@mui/icons-material/Home';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import ChatIcon from '@mui/icons-material/Chat';
+import GroupIcon from '@mui/icons-material/Group';
+
+function BottomNavBar(){
+    return<Box
+        display="flex"
+        flexDirection="row"
+        gap={2}
+        padding={3}
+    >
+        <Link to="/createEvent">
+            <Button variant="contained">
+                <HomeIcon/>
+            </Button>
+        </Link>
+        <Link to="/createEvent">
+            <Button variant="contained">
+                <CalendarMonthIcon/>
+            </Button>
+        </Link>
+        <Link to="/friendsPage">
+            <Button variant="contained">
+                <ChatIcon/>
+            </Button>
+        </Link>
+        <Link to="/friendsPage">
+            <Button variant="contained">
+                <GroupIcon/>
+            </Button>
+        </Link> 
+    </Box>
+}
+
+export default BottomNavBar;
