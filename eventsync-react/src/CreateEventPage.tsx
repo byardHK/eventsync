53e60807c9bfb6649eb37c31854c1f9a3bb33388
aharnly18 <amharnly@msn.com>
2025-01-15 20:07:30 -0500

    UI and post done but not pretty

--- eventsync-react/src/CreateEventPage.tsx
+++ eventsync-react/src/CreateEventPage.tsx
@@ -9,6 +9,9 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { JSX } from "react/jsx-runtime";
 import { MobileDateTimePicker } from "@mui/x-date-pickers/MobileDateTimePicker";
+import Checkbox from "@mui/material/Checkbox";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
 
 function CreateEventPage() {
     const [titleText, setTitleText] = useState<String>("");    
@@ -16,19 +19,25 @@ function CreateEventPage() {
     const [endDateTime, setEndDateTime] = useState<Date | null>(null); 
     const [locationText, setLocationText] = useState<String>("");      
     const [tags, setTags] = useState<string[]>([]); 
+    const [checked, setChecked] = useState<boolean>(false);
+    const [recurFrequency, setRecurFrequency] = useState<string>("Weekly"); 
+    const [endRecurDateTime, setEndRecurDateTime] = useState<Date | null>(null); 
 
     const handleSubmit = async (e: React.FormEvent<HTMLButtonElement>) => {
 
         e.preventDefault();
         try {
+            const postPath = checked ? 'http://localhost:5000/post_recurring_event' : 'http://localhost:5000/post_event';
             const data = {
                 "title": titleText,
                 "startDateTime": startDateTime,
                 "endDateTime": endDateTime,
                 "locationName": locationText,
-                "tags": tags
+                "tags": tags,
+                "recurFrequency": recurFrequency,
+                "endRecurDateTime": endRecurDateTime
             }
-            const response = await fetch('http://localhost:5000/post_event', {
+            const response = await fetch(postPath, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -49,6 +58,14 @@ function CreateEventPage() {
         }
           
     };
+
+    function onCheckBoxChange() {
+        setChecked(!checked);
+    }
+
+    function handleRecurFrequencyChange(newFrequency: string) {
+        setRecurFrequency(newFrequency);
+    }
 
     return <>
         
@@ -93,7 +110,7 @@ function CreateEventPage() {
                         type="text" 
                         value={locationText} 
                         onChange={(event) => setLocationText(event.target.value)}  
-                    />
+                    />    
                 </Box>
             </LocalizationProvider>
             <Autocomplete
@@ -110,6 +127,41 @@ function CreateEventPage() {
                 sx={{ width: '500px' }}
                 onChange={(_event, value) => { setTags(value);}} 
             />
+            <Box>
+                <Checkbox
+                    checked={checked}
+                    onChange={onCheckBoxChange}
+                    
+                />
+                {checked ? 
+                    <div>
+                        <Select
+                            value={recurFrequency}
+                            label="Frequency"
+                            // onChange={handleRecurFrequencyChange("hi")}
+                        >
+                            <MenuItem value={"Daily"}>Daily</MenuItem>
+                            <MenuItem value={"Weekly"}>Weekly</MenuItem>
+                            <MenuItem value={"Monthly"}>Monthly</MenuItem>
+                        </Select>
+                        <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                <Box display="flex" 
+                                    alignItems="center" 
+                                    justifyContent="center"
+                                    component="form"
+                                    sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
+                                    noValidate
+                                    autoComplete="off">
+                                <MobileDateTimePicker
+                                    label="End Date"
+                                    value={endRecurDateTime}
+                                    onChange={(newValue) => setEndRecurDateTime(newValue)} 
+                                />
+                                </Box>
+                            </LocalizationProvider>
+                    </div>
+                : null}
+            </Box>
             <Button variant="contained" onClick={(e) => handleSubmit(e)}>Submit</Button>
             
 
