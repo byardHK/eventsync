02fc4b9654268672430d92cf9e475f05f406f121
aharnly18 <amharnly@msn.com>
2025-02-16 22:24:39 -0500

    Overhaul chat code, but now it works!

--- eventsync-react/src/pages/ChatPage.tsx
+++ eventsync-react/src/pages/ChatPage.tsx
@@ -1,89 +1,97 @@
-import Pusher, { Channel } from "pusher-js";
-import { useState, useEffect } from 'react';
-import axios from "axios";
+import React, {useEffect} from 'react';
 import { Button, TextField, Box } from "@mui/material";
-import { useUser } from '../sso/UserContext';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import { useNavigate, useParams } from 'react-router-dom';
+import axios from "axios";
 import Message from '../types/Message';
+import { useState } from 'react';
+import { useUser } from '../sso/UserContext';
+import { useNavigate, useParams } from 'react-router-dom';
 
 
+//Pusher
+import Pusher from 'pusher-js';
 function ChatPage() {
-    const { userDetails } = useUser();
-    const { chatId } = useParams<{ chatId: string }>();
-    // const chatId = parseInt(strChatId || '-1');
-    const userId = userDetails.email ? userDetails.email : "";
-    const channelName = `chat-channel-${chatId}`; // TODO: make this dynamic
     const [chats, setChats] = useState<Message[]>([]);
+    const { chatId } = useParams<{ chatId: string }>() ;
     const [msg, setMsg] = useState<Message>();
-    // const [pusherChannel, setPusherChannel] = useState<Channel | null>(null);
+    const { userDetails } = useUser();
+    const userId = userDetails.email ? userDetails.email : "";
 
-    const navigate = useNavigate();
+
+	//This will be called when your component is mounted
+	useEffect(() => {
+		const pusher = new Pusher('d2b56055f7edd36cb4b6', {
+			cluster: 'us2'
+		})
+		const channel1 = pusher.subscribe('channel_name1');
+		// You can bind more channels here like this
+		// const channel2 = pusher.subscribe('channel_name2')
+		channel1.bind('private_channel_id', function(data: Message) {
+            setMsg(data);
+            console.log(data);
+		    // Code that runs when channel1 listens to a new message
+		});
+        channel1.bind("pusher:subscription_succeeded", retrieveHistory);
+		
+		return (() => {
+			pusher.unsubscribe('channel_name1')
+			// pusher.unsubscribe('channel_name2')
+		})
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+	// [] would ensure that useEffect is executed only once
 
     useEffect(() => {
-        const pusher = new Pusher('d2b56055f7edd36cb4b6', {
-            cluster: 'us2'
-            //   encrypted: true,
-        });
-        const channel = pusher.subscribe(channelName);
-        channel.bind("new-message", (data: Message) => {
-            setMsg(data);
-            console.log(`I got a new message! : ${data.messageContent}`);
-        });
-        channel.bind("pusher:subscription_succeeded", retrieveHistory);
-        return () => {
-            pusher.unsubscribe(channelName);
-        };
-    });
-
-    useEffect(() => {          
-        // if (msg) setChats([...chats, msg].sort((a: Message, b: Message) => {
-        //     if (a.timeSent > b.timeSent) return -1;
-        //     else if (a.timeSent < b.timeSent) return 1;
-        //     else return 0;
-        // }));
-        console.log(chats.length);
-    }, [msg]);
-
-    // function addMessage(message: Chat) {
-    //     console.log(`I will set chats now: ${chats.length}; ${message.messageContent}`);
-    //     setChats([...chats, message]);
-    //     console.log(`Chats have been set: ${chats.length}`);
-
-    // }
+        if(msg) {
+            setChats([...chats,msg]);
+            console.log(chats.length);
+        }
+      }, [msg]);
 
     async function retrieveHistory() {
-        const response = await axios.get<ChatList>(`http://localhost:5000/get_chat_hist/${chatId}`);
+        const response = await axios.get<MessageList>(`http://localhost:5000/get_chat_hist/${chatId}`);
         setChats(response.data.chats);
     }
 
-    const handleBackClick = () => {
-        navigate('/chatHomePage');
-    };
+	return(
+		<div>
+            <ChatList chats={chats} userId={'harnlyam20@gcc.edu'}></ChatList>
+            <ChatInput channelName={""} userId={userId} chatId={chatId ?? "-1"}></ChatInput>
+		</div>
+	)
+}
 
+const ChatList = (prop: { chats: Message[], userId: String }) => {
     return (
-        <div>
-            <Button onClick={handleBackClick} title="go to My Events page">
-                <ArrowBackIcon />
-             </Button>
-            <ChatList chats={chats} userId={userId} />
-            <ChatInput channelName={channelName} userId={userId} chatId={chatId ? chatId : "-1"} />
+        <div style={{}}>
+            {prop.chats.map((chat) => {
+                return (
+                    <Box
+                        style={{ width: '50%', display: "flex" }}
+                        >
+                        <Box
+                            style={chat.senderId == prop.userId ? { right: '0', backgroundColor: 'white', alignItems: 'baseline'} : 
+                            {   backgroundColor: '#90D5FF' }}
+                            >
+                            {chat.messageContent}<br></br>
+                        </Box>
+                    </Box>
+                );
+            })}
         </div>
     );
 };
 
-
-const ChatInput = (prop: { channelName: String, userId: String, chatId: string }) => {
+const ChatInput = (props: { channelName: String, userId: String, chatId: string }) => {
     const [message, setMessage] = useState<string>("");
 
     const sendMessage = () => {
         // e.preventDefault(); // I should do this
         if (message.trim().length > 0) {
-            var data: Message = {
-                senderId: prop.userId,
+            const data = {
+                senderId: props.userId,
                 messageContent: message,
                 id: -1, // TODO: how do I want to do this?
-                chatId: Number(prop.chatId),
+                chatId: props.chatId,
                 timeSent: getCurDate()
             };
             axios.post(`http://localhost:5000/message/`, data);
@@ -114,35 +122,13 @@ const ChatInput = (prop: { channelName: String, userId: String, chatId: string }
     );
 };
 
-const ChatList = (prop: { chats: Message[], userId: String }) => {
-    return (
-        <div style={{}}>
-            {prop.chats.map((chat) => {
-                return (
-                    <Box
-                        style={{ width: '50%', display: "flex" }}
-                        >
-                        <Box
-                            style={chat.senderId == prop.userId ? { right: '0', backgroundColor: 'white', alignItems: 'baseline'} : 
-                            {   backgroundColor: '#90D5FF' }}
-                            >
-                            {chat.messageContent}<br></br>
-                        </Box>
-                    </Box>
-                );
-            })}
-        </div>
-    );
-};
-
-type ChatList = {
-    chats: Message[]
-}
-
 function getCurDate() {
     const now = new Date();
-    return `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(2, '0')}-${String(now.getDate()).padStart(2, '0')} 
-        ${String(now.getHours()).padStart(2, '0')}:${String(now.getMinutes()).padStart(2, '0')}:${String(now.getSeconds()).padStart(2, '0')}`; 
+    return `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(2, '0')}-${String(now.getDate()).padStart(2, '0')} ${String(now.getHours()).padStart(2, '0')}:${String(now.getMinutes()).padStart(2, '0')}:${String(now.getSeconds()).padStart(2, '0')}`; 
+}
+
+type MessageList = {
+    chats: Message[]
 }
 
 export default ChatPage;
