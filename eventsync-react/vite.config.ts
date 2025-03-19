54099c155db4564c336b05bc5c429a8dc1d67b4c
jminnich23 <joshminnich3@gmail.com>
2025-03-19 00:20:30 -0400

    more vite attempts

--- eventsync-react/vite.config.ts
+++ eventsync-react/vite.config.ts
@@ -5,7 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     port: 5173, 
     open: true, 
+    proxy: {
+      '/api': {
+        target: 'http://eventsync.gcc.edu:3000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 })
