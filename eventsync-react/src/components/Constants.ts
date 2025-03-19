f7fe034d48eb63de256ef6d4a89663adbbfea80a
jminnich23 <joshminnich3@gmail.com>
2025-03-18 23:43:00 -0400

    constants

--- eventsync-react/src/components/Constants.ts
+++ eventsync-react/src/components/Constants.ts
@@ -1,7 +1,7 @@
 // change this to be http://localhost:5000 for testing. Production should be https://eventsync.gcc.edu
 
 //Prod
-export const BASE_URL: string = "https://eventsync.gcc.edu:5000";
+export const BASE_URL: string = "https://eventsync.gcc.edu:3000";
 
 export const REDIRECT_URI: string = "https://eventsync.gcc.edu:5173";
 
