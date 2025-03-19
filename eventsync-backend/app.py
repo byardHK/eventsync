67b5493967b062abbfeb5f473d6f89c309a4d4b7
jminnich23 <joshminnich3@gmail.com>
2025-03-19 00:34:16 -0400

    more attempts

--- eventsync-backend/app.py
+++ eventsync-backend/app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 app.config["PROPAGATE_EXCEPTIONS"] = True  # Ensure exceptions are raised
 
-CORS(app)
+CORS(app, origins=["http://10.18.101.62:5173"])
 
 db_config = {
     'host': '10.18.101.62',  
