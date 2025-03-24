fd052b05a9fc71a0c358116748753ef80b1cc6b8
jminnich23 <joshminnich3@gmail.com>
2025-03-23 22:07:51 -0400

    running VM dev

--- eventsync-react/src/components/Constants.ts
+++ eventsync-react/src/components/Constants.ts
@@ -1,9 +1,9 @@
 // change this to be http://localhost:5000 for testing. Production should be https://eventsync.gcc.edu
 
 //Prod
-export const BASE_URL: string = "https://eventsync.gcc.edu";
+export const BASE_URL: string = "https://eventsync.gcc.edu:5000";
 
-export const REDIRECT_URI: string = "https://eventsync.gcc.edu";
+export const REDIRECT_URI: string = "https://eventsync.gcc.edu:3000";
 
 //Testing
 // export const BASE_URL: string = "http://localhost:5000";
