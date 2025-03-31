8da67ada5e6165c40f49b65035975ea3ca8958b1
jminnich23 <joshminnich3@gmail.com>
2025-03-30 23:09:04 -0400

    port back to 443

--- eventsync-react/vite.config.ts
+++ eventsync-react/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
       key: fs.readFileSync(path.resolve(__dirname, '../eventsync-backend/server.key')),
       cert: fs.readFileSync(path.resolve(__dirname, '../eventsync-backend/server.crt')), 
     },
-    port: 3000,
+    port: 443,
     proxy: {
       '/api': {
         target: 'https://eventsync.gcc.edu:5000', // Ensure backend is running HTTPS
