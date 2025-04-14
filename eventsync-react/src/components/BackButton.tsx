f5e10fe366596af48e2dc56bdf59d81b13cd0e67
jminnich23 <joshminnich3@gmail.com>
2025-04-14 10:35:06 -0400

    back button should actually work

--- eventsync-react/src/components/BackButton.tsx
+++ eventsync-react/src/components/BackButton.tsx
@@ -2,12 +2,13 @@ import { Box, Button } from "@mui/material"
 import { useNavigate } from "react-router-dom";
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { REDIRECT_URI } from './Constants';
+import { useLocation } from "react-router-dom";
 
 function BackButton() {
     const navigate = useNavigate();
 
     const handleBackClick = () => {
-        if (window.performance?.navigation?.type === 1 || document.referrer === "") {
+        if (navigate.length == 0) {
             navigate('/'); 
         } else {
             navigate(-1);
